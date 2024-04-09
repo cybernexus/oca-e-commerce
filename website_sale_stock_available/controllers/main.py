6e3c665eba16a45d71090fe51063b5f48e87e5fc
Pete Snyder <cybernexus00@yahoo.com>
2024-04-09 11:22:30 -0400

    [FIX] website_sale_stock_available: module controller should inherit from website_sale now instead of website_sale_stock

--- website_sale_stock_available/controllers/main.py
+++ website_sale_stock_available/controllers/main.py
@@ -2,11 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo.http import request, route
 
-from odoo.addons.website_sale_stock.controllers.main import PaymentPortal
+from odoo.addons.website_sale.controllers.main import PaymentPortal
 
 
 class PaymentPortal(PaymentPortal):
-    @route()
+
+    @route(
+        '/shop/payment/transaction/<int:order_id>', type='json', auth='public', website=True
+    )
     def shop_payment_transaction(self, *args, **kwargs):
         """Inject a context when potential or promised stock is set"""
         request.website = request.website.with_context(
