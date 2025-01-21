9765b586f0e90b0ab597348d47648f011e9b36c4
Pete Snyder <cybernexus00@yahoo.com>
2025-01-21 17:27:19 -0500

    [FIX] website_sale_stock_available: resolves issue with method signature mismatch

--- website_sale_stock_available/models/sale_order.py
+++ website_sale_stock_available/models/sale_order.py
@@ -7,6 +7,6 @@ from odoo import models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    def _cart_update(self, *args, **kwargs):
+    def _cart_update(self, product_id, line_id=None, add_qty=0, set_qty=0, **kwargs):
         order = self.with_context(website_sale_stock_available=True)
-        return super(SaleOrder, order)._cart_update(*args, **kwargs)
+        return super(SaleOrder, order)._cart_update(product_id, line_id, add_qty, set_qty, **kwargs)
