7ce07d5f8c9e26be2a6aed1283e6f9f3511ec828
Pete Snyder <cybernexus00@yahoo.com>
2025-05-05 05:55:42 -0400

    [FIX] website_sale_stock_available: override availability dict instead of base core dict to resolve circular references

--- website_sale_stock_available/models/product_product.py
+++ website_sale_stock_available/models/product_product.py
@@ -7,14 +7,11 @@ from odoo import models
 class Product(models.Model):
     _inherit = "product.product"
 
-    def _compute_quantities_dict(
-        self, lot_id, owner_id, package_id, from_date=False, to_date=False
-    ):
-        res = super()._compute_quantities_dict(
-            lot_id, owner_id, package_id, from_date, to_date
-        )
+    def _compute_available_quantities_dict(self):
+        res = super()._compute_available_quantities_dict()
+
         if self.env.context.get("website_sale_stock_available"):
             for product in self.with_context(website_sale_stock_available=False):
                 immediately = product.immediately_usable_qty
+                product.free_qty = immediately
                 res[product.id]["free_qty"] = immediately
-        return res
